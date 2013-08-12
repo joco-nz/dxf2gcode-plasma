90653fc33cb668f3b6cd9ed685d66bf38a1122d6
Jean-Paul Schouwstra <jp1357@gmail.com>
2013-08-12 12:46:39 +0000

    Added new wiki file DevelopmentEnvironment_Win7.wiki; removed outdated files, and the empty folder postpro_config

--- source/make_exe.py
+++ source/make_exe.py
@@ -5,15 +5,15 @@ import subprocess
 
 pyinpfad = "C:\Python27\pyinstaller-2.0\pyinstaller.py"
 
-pyt = "C:/Python27/pythonw.exe"
+pyt = "C:\Python27\pythonw.exe"
 filepfad= os.path.realpath(os.path.dirname(sys.argv[0]))
 exemakepfad=filepfad
 file = "dxf2gcode"
-icon= "%s/DXF2GCODE-001.ico" %filepfad
+icon= "%s\DXF2GCODE-001.ico" %filepfad
 upxdir="C:\Python27\pyinstaller-2.0\upx309w"
 
-options=("--noconsole --upx-dir=%s --icon=%s" %(upxdir, icon))
-options=("--noconsole --icon=%s" %(icon))
+#options=("--noconsole --upx-dir=%s --icon=%s" %(upxdir, icon)) #uncomment to use uxp
+options=("--noconsole --icon=%s" %(icon)) #comment to use uxp
 print options
 
 #Verzwichniss wechseln
@@ -22,13 +22,13 @@ exemakepfad = unicode( exemakepfad, "utf-8" )
 os.chdir(exemakepfad.encode( "utf-8" ))
 
 
-cmd=("%s %s %s %s/%s.py" %(pyt,pyinpfad,options,filepfad,file))
+cmd=("%s %s %s %s\%s.py" %(pyt,pyinpfad,options,filepfad,file))
 print cmd
 retcode=subprocess.call(cmd)
-#
+
 #cmd=("%s %s\Build.py %s\%s.spec" %(pyt,pyinpfad,exemakepfad,file))
 #print cmd
 #retcode=subprocess.call(cmd)
 
-print "\n!!!!!!!Bitmaps and Languages folder not vergessen!!!!!!"
-print "\nReady"
+print "\n!!!!!!!Do not forget the Bitmap and Language folder!!!!!!"
+print "\nREADY"
