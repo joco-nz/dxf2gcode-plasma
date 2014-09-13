fc816b338d16c6c9fc8f543d649d50907752d45f
Christian Kohlöffel <christian-kohloeffel@t-online.de>
2014-09-13 11:07:54 +0000

    Replaced make_exe by make_exe_tm.py

--- source/make_exe.py
+++ source/make_exe.py
@@ -7,6 +7,8 @@ import subprocess
 pyinpfad = "C:/Python27/pyinstaller-2.0/pyinstaller.py"
 
 pyt = "C:/Python27/pythonw.exe"
+filepfad = os.path.realpath(os.path.dirname(sys.argv[0]))
+exemakepfad = filepfad
 file_ = "dxf2gcode"
 icon = "%s/dxf2gcode_pyQt4_ui/images/DXF2GCODE-001.ico" % filepfad
 upxdir = "C:/Python27/pyinstaller-2.0/upx309w"
