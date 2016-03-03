143272b4e3df8be2a809bfa774c6185ec88e504b
neveruml <neveruml@users.sourceforge.net>
2016-03-03 22:53:03 +0100

    * Merged "xi_config" into "develop" branch

--- source/gui/configwindow.py
+++ source/gui/configwindow.py
@@ -480,7 +480,7 @@ class ConfigWindow(QDialog):
                 if subsection == '__section_title__':
                     #skip the special section
                     continue
-                
+
                 #Browse sublevels
                 self.createWidgetSubSectionWithSubLevels(subdefinition[subsection], section_widget) #Recursive call, all the nested configuration item will appear at the same level
         else:
@@ -522,7 +522,7 @@ class ConfigWindow(QDialog):
             #skip the special section __section_title__
             if section == '__section_title__' or isinstance(window_def[section], CfgDummy):
                 continue
-            
+
             if config is not None and section in config:
                 if isinstance(window_def[section], dict):
                     #Browse sublevels
@@ -561,19 +561,19 @@ class ConfigWindow(QDialog):
         minimum = None
         maximum = None
         string_list = []
-        
+
         if isinstance(configspec, dict):
             #If the received configspec is a dictionary, we most likely have a table, so we are going to exctract sections names of this table
-            
+
                 #When tables are used, the "__many__" config entry is used for the definition of the configspec, so we try to excract the sections names by using this __many__ special keyword.
                 #Example: 'Tool_Parameters': {[...], '__many__': {'diameter': 'float(default = 3.0)', 'speed': 'float(default = 6000)', 'start_radius': 'float(default = 3.0)'}}
                 if '__many__' in configspec and isinstance(configspec['__many__'], dict):
                     string_list = configspec['__many__'].keys()
                     string_list.insert(0, '') #prepend an empty element since the first column of the table is the row name (eg a unique tool number)
-        
+
         else:
             #configspec is normaly a string from which we can exctrat min / max values and possibly a list of options
-            
+
             #Handle "option" config entries
             string_list = self.configspecParserExctractSections('option', configspec)
             i = 0
@@ -587,9 +587,9 @@ class ConfigWindow(QDialog):
                     #unwanted item, it doesn't contain an element of the option()
                     del string_list[i]
                     continue
-                    
+
                 i += 1
-            
+
             #Handle "integer" and "string" config entries
             if len(string_list) <= 0:
                 string_list = self.configspecParserExctractSections('integer', configspec)
@@ -612,9 +612,9 @@ class ConfigWindow(QDialog):
             
             comments_string = comments_string.strip(' #')
             comments_string = comments_string.replace('#', '\n')
-        
+
         logger.debug('configspecParser(): exctracted option elements = {0}, min = {1}, max = {2}, comment = {3}'.format(string_list, minimum, maximum, comments_string))
-        
+
         result = {}
         result['minimum'] = minimum
         result['maximum'] = maximum
@@ -657,16 +657,16 @@ class ConfigWindow(QDialog):
         returns a list of item from a string. Eg the string "option('mm', 'in', default = 'mm')" will be exploded into the string list ["mm", "in", "default = 'mm'"]
         """
         string_list = []
-        
+
         pos_init = string.find(attribute_name + '(')
         if pos_init >= 0:
             pos_init += len(attribute_name + '(') #skip the "option("
-            
+
             pos_end = string.find(')', pos_init)
             if pos_end > pos_init:
                 #print("substring found = {0}".format(string[pos_init:pos_end]))
                 string_list = string[pos_init:pos_end].split(',')
-        
+
         # remove empty elements and remove leading and trailing spaces
         string_list = [string.strip() for string in string_list if string]
         return string_list
@@ -696,7 +696,7 @@ class ConfigWindow(QDialog):
             #skip the special section __section_title__
             if section == '__section_title__' or isinstance(window_def[section], CfgDummy):
                 continue
-            
+
             if isinstance(window_def[section], dict):
                 #Browse sublevels
                 (result_bool, result_string) = self.validateConfiguration(window_def[section], result_string, result_bool) #Recursive call, until we find a real item (not a dictionnary with subtree)
@@ -709,7 +709,7 @@ class ConfigWindow(QDialog):
                 else:
                     #Item should be a layout or a widget
                     logger.warning("item {0} is not a widget, can't validate it!".format(window_def[section]))
-        
+
         return (result_bool, result_string)
 
 
@@ -964,13 +964,9 @@ class CorrectedDoubleSpinBox(QDoubleSpinBox):
         QDoubleSpinBox.setSuffix(self, suffix)
 
     def valueFromText(self, text):
-        if c.PYQT5notPYQT4:
-            #print("valueFromText({0})".format(text.encode('utf-8')))
-            text = text.encode('utf-8').replace(self.saved_suffix.encode('utf-8'), '').replace(QLocale().decimalPoint(), '.')
-        else:
-            #print("valueFromText({0})".format(text))
-            text = text.replace(self.saved_suffix, '').replace(QLocale().decimalPoint(), '.')
-        return toFloat(text)[0]
+        # result = float(text.replace('.', QLocale().decimalPoint()))
+        # python expect a dot ('.') as decimal separator
+        text = qstr_encode(text).replace(str_encode(self.saved_suffix), '').replace(str(QLocale().decimalPoint()), '.')
 
     def validate(self, entry, pos):
         #let's *really* trust the validator
@@ -1049,10 +1045,10 @@ class CfgLineEdit(CfgBase):
         """
         if spec['minimum'] is not None:
             self.size_min = spec['minimum']
-        
+
         if spec['maximum'] is not None:
             self.lineedit.setMaxLength(spec['maximum'])
-        
+
         if spec['comment']:
             self.setWhatsThis(spec['comment'])
 
@@ -1248,7 +1244,7 @@ class CfgComboBox(CfgBase):
         self.combobox.blockSignals(True) # Avoid unnecessary signal (we don't want the config window to emit any signal when filling the fields programatically)
         self.combobox.clear()
         self.combobox.addItems(spec['string_list'])
-        
+
         if spec['comment']:
             self.setWhatsThis(spec['comment'])
         self.combobox.blockSignals(False)
@@ -1298,7 +1294,7 @@ class CfgTable(QWidget):
         @param columns: string list containing all the columns names
         """
         QWidget.__init__(self, parent)
-        
+
         self.tablewidget = QTableWidget(parent)
         self.tablewidget.setSelectionBehavior(QAbstractItemView.SelectRows)
         if isinstance(columns, (list, tuple)):
@@ -1307,7 +1303,7 @@ class CfgTable(QWidget):
             self.keys = [] #No columns yet
         self.tablewidget.horizontalHeader().setStretchLastSection(True)
         self.tablewidget.horizontalHeader().sectionClicked.connect(self.tablewidget.clearSelection) #Allow to unselect the lines by clicking on the column name (useful to add a line at the end)
-        
+
         self.label = QLabel(text, parent)
         self.button_add = QPushButton(QIcon(QPixmap(":/images/list-add.png")), "")
         self.button_remove = QPushButton(QIcon(QPixmap(":/images/list-remove.png")), "")
@@ -1316,18 +1312,18 @@ class CfgTable(QWidget):
         self.layout_button = QVBoxLayout()
         self.layout_button.addWidget(self.button_add)
         self.layout_button.addWidget(self.button_remove)
-        
+
         self.layout_table = QHBoxLayout()
         #self.tablewidget.setSizePolicy(size_policy)
         self.layout_table.addWidget(self.tablewidget)
         self.layout_table.addLayout(self.layout_button)
-        
+
         self.layout = QVBoxLayout(parent)
-        
+
         self.layout.addWidget(self.label)
         self.layout.addLayout(self.layout_table)
         self.setLayout(self.layout)
-        
+
         #Ensure that the table always expand to the maximum available space
         size_policy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         size_policy.setVerticalStretch(10)
@@ -1344,7 +1340,7 @@ class CfgTable(QWidget):
             self.keys[0] = 'name' #name of first column is normaly undefined in configspec, so we use a generic name, just to display something in the header of the QTable
         self.tablewidget.setColumnCount(len(self.keys))
         self.tablewidget.setHorizontalHeaderLabels(self.keys)
-        
+
         if spec['comment']:
             self.setWhatsThis(spec['comment'])
 
@@ -1367,9 +1363,9 @@ class CfgTable(QWidget):
         selected_row = self.tablewidget.currentRow()
         if selected_row < 0 or len(self.tablewidget.selectedIndexes()) <= 0: #Trick to be able to insert lines before the first and after the last line (click on column name to unselect the lines)
             selected_row = self.tablewidget.rowCount()
-            
+
         self.tablewidget.insertRow(selected_row)
-        
+
         #If provided, fill the table with the content of the line list
         if line is not None and isinstance(line, (list, tuple)) and len(line) >= self.tablewidget.columnCount():
             for i in range(self.tablewidget.columnCount()):
@@ -1394,7 +1390,7 @@ class CfgTable(QWidget):
         selected_row = self.tablewidget.currentRow()
         if selected_row < 0 and self.tablewidget.rowCount() > 0:
             selected_row = self.tablewidget.rowCount() - 1
-            
+
         if selected_row >= 0:
             self.tablewidget.removeRow(selected_row)
 
@@ -1431,7 +1427,7 @@ class CfgTable(QWidget):
             if not key:
                 continue
             result_dict[key] = {}
-            
+
             j = 1
             while j < self.tablewidget.columnCount():
                 sub_key = self.keys[j] #Column name
@@ -1457,7 +1453,7 @@ class CfgTable(QWidget):
         if isinstance(value, dict) and len(self.keys) > 0:
             self.tablewidget.setRowCount(0)
             line = [None] * len(self.keys)
-            
+
             #sort according to the key
             item_list=[]
             try:
@@ -1466,10 +1462,10 @@ class CfgTable(QWidget):
             except ValueError:
                 #fallback to standard sort
                 item_list = sorted(value.keys())
-            
+
             for item in item_list:
                 line[0] = item #First column is alway the key of the dict (eg it can be the tool number)
-                
+
                 #Compute the other columns (the received value must contain a dict entry for each column)
                 i = 1
                 while i < len(self.keys):
@@ -1479,14 +1475,14 @@ class CfgTable(QWidget):
                         result = False
                         break
                     i += 1
-                
+
                 if result is True:
                     self.appendLine(line)
         else:
             result = False
-        
+
         self.tablewidget.blockSignals(False)
-        
+
         return result
 
 
@@ -1539,7 +1535,7 @@ class CfgTableCustomActions(CfgTable):
         result_string = ''
         result_bool = True
         keys_list = []
-        
+
         if self.tablewidget.rowCount() > 0 and self.tablewidget.columnCount() > 0:
             for i in range(self.tablewidget.rowCount()):
                 if not self.tablewidget.item(i, 0) or not self.tablewidget.item(i, 0).text():
@@ -1554,7 +1550,7 @@ class CfgTableCustomActions(CfgTable):
                 #There are duplicate entries, that's wrong because the key must be unique
                 result_bool = False
                 result_string += str(self.tr('\nFound {0} duplicate elements for the table "{1}"\n')).format(nb_duplicate_elements, self.label.text())
-            
+
         return (result_bool, result_string)
 
     def getValue(self):
@@ -1573,7 +1569,7 @@ class CfgTableCustomActions(CfgTable):
             if not key:
                 continue
             result_dict[key] = {}
-            
+
             #Get the values (other columns)
             for j in range(1, self.tablewidget.columnCount()):
                 sub_key = self.keys[j] #Column name
@@ -1583,7 +1579,7 @@ class CfgTableCustomActions(CfgTable):
                 value = str(value.toPlainText())
                 if sub_key is not None:
                     result_dict[key][sub_key] = value
-        
+
         return result_dict
 
 
@@ -1620,14 +1616,14 @@ class CfgTableToolParameters(CfgTable):
             spinbox = QSpinBox()
             spinbox.setMinimum(0)
             spinbox.setMaximum(1000000000) #Default value is 99
-            
+
             converted_value = toInt(value)  # Convert the value to int (we may receive a string for example)
             computed_value = converted_value[0] if converted_value[1] else self.max_tool_number + 1
             self.max_tool_number = max(self.max_tool_number, computed_value) #Store the max value for the tool number, so that we can automatically increment this value for new tools
             spinbox.setValue(computed_value) #first column is the key, it must be an int
-        
+
         spinbox.valueChanged.connect(self.valueChangedSlot) #We need to detect any change in the table's widgets
-        
+
         self.tablewidget.setCellWidget(line, column, spinbox)
 
     def valueChangedSlot(self):
@@ -1648,7 +1644,7 @@ class CfgTableToolParameters(CfgTable):
         result_string = ''
         result_bool = True
         keys_list = []
-        
+
         if self.tablewidget.rowCount() > 0 and self.tablewidget.columnCount() > 0:
             for i in range(self.tablewidget.rowCount()):
                 if not self.tablewidget.cellWidget(i, 0):
@@ -1659,7 +1655,7 @@ class CfgTableToolParameters(CfgTable):
                     keys_list.append(str(self.tablewidget.cellWidget(i, 0).value()))
                     if self.tablewidget.cellWidget(i, 0).value() == 1:
                         contains_tool_1 = True
-            
+
             nb_duplicate_elements = len(keys_list) - len(set(keys_list))
             if nb_duplicate_elements != 0:
                 #There are duplicate entries, that's wrong because the key must be unique
@@ -1688,7 +1684,7 @@ class CfgTableToolParameters(CfgTable):
             if not key:
                 continue
             result_dict[key] = {}
-            
+
             #Get the values (other columns)
             for j in range(1,self.tablewidget.columnCount()):
                 sub_key = self.keys[j] #Column name
@@ -1698,5 +1694,5 @@ class CfgTableToolParameters(CfgTable):
                 value = value.value()
                 if sub_key is not None:
                     result_dict[key][sub_key] = value
-        
+
         return result_dict
